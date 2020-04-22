3797d7bec85f2e3b231c2e41903184e1dda73fc0
Safyan Iqbal <safyan23@Safyans-MacBook-Pro.local>
2020-04-21 17:03:14 -0700

    Finished Lab Assignment 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ToastService } from './toast/toast.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toast-app';
+  toastTypes: Array<string> = [];
+  constructor(private toastService: ToastService) {
+    this.toastTypes = ['success', 'info', 'warning', 'danger'];
+  }
+
+  // success, info, warning, danger
+showToast() {
+  const rand = Math.floor(Math.random() * 4);
+  console.log('My random number is: ' + rand);
+  const toastType = this.toastTypes[rand];
+  const toastMessage = 'Hi this is a message my random number is: ' + rand;
+  const duration = 4000;
+  this.toastService.showToast(toastType, toastMessage, duration);
+}
 }
